80e784a470cea8f7940a04e593ae2eee9bb96ed8
Abhinand Arun <abhinandarun2015@gmail.com>
2022-11-27 00:15:17 +0530

    Add Hero Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './Components/NavBar/Navbar';
-// import 
+import Hero from './Components/Home/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/Components/Home/Hero.jsx
@@ -0,0 +1,18 @@
+import "./hero.css"
+import image from "../../assets/cover.jpg"
+const hero = () => {
+    return (
+        <div class="banner-slide">
+            <div class="banner-overlay">
+                <div class="banner-content">
+                    <h1>A Better Life For India's<br />Stray Animals</h1>
+                    <p>A little bit of space for a subheading <br />again over two lines.</p>
+                    <a href="#" class="button">Donate</a>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default hero;
