1a739fbd5f05248e0794c06e9fb4721e417efc53
Abhinand Arun <abhinandarun2015@gmail.com>
2022-11-27 14:03:08 +0530

    Update Data in Donate Card

--- src/Components/Donate/Container.jsx
+++ src/Components/Donate/Container.jsx
@@ -1,14 +1,23 @@
 import DCard from "./DonationCard";
 
 const Container = () => {
-    const data = [{}]
-    return ( 
+    const data = [{
+        title: "Be for pets",
+        caption: "Get started with small steps",
+        desc: "The earth is not just for humans. With every rupee you Donate, it helps some animal in need. Donate now and help us save the animals."
+    }, {
+        title: "Be for pets",
+        caption: "Get started with small steps",
+        desc: "The earth is not just for humans. With every rupee you Donate, it helps some animal in need. Donate now and help us save the animals."
+    }]
+    return (
         <div className="py-8 lg:px-40">
-            <DCard />
-            <DCard />
-            <DCard />
+            {data.map((item, index) => {
+                return <DCard key={index} title={item.title} caption={item.caption} desc={item.desc} />
+            }
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default Container;
