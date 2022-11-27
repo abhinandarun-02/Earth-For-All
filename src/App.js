dc27cee14631fe4c6a0004bfa93686b2e8874f5d
Abhinand Arun <abhinandarun2015@gmail.com>
2022-11-27 07:35:27 +0530

    Add service section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Components/NavBar/Navbar';
 import Hero from './Components/Home/Hero';
 import Info from './Components/Home/Info';
+import Services from './Components/Home/Services';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Info />
+      <Services />
     </div>
   );
 }
