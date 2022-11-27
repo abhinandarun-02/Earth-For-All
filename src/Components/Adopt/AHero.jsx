7ae3f51e320057783e4cda6b30747bc641843325
Abhinand Arun <abhinandarun2015@gmail.com>
2022-11-27 13:30:49 +0530

    Add Rescue Page

--- src/Components/Adopt/AHero.jsx
+++ src/Components/Adopt/AHero.jsx
@@ -1,13 +1,13 @@
 import './ahero.css'
 
-const AHero = () => {
+const AHero = (props) => {
     return (
         <div class="main-body-section-div text-white relative">
 				<div class="heading-main-body">
 					<div class="main-body-heading-text absolute inset-0 flex items-center justify-center flex-col gap-4">
-						<h1 class="heading-main-body-h1">Adopt Me</h1>
-						<div class="heading-main-body-moto text-[20px] font-medium px-[30%]">
-                        India’s stray animal population numbers nearly 90 million. However, India’s shelter system already faces population stress with less than 9 million animals. SAFI’s work finding forever homes for these neglected animals not only gives each pet the love they deserve but frees up critically important housing and veterinary capacity for those remaining in shelters.
+						<h1 class="heading-main-body-h1">{props.title}</h1>
+						<div class="heading-main-body-moto text-[20px] font-medium px-[30%]">{props.desc}
+	                        
 						</div>
 					</div>
 				</div>
