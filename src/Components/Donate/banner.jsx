c11ce9555d31aea8fa1349aff411dccfdde3303b
Abhinand Arun <abhinandarun2015@gmail.com>
2022-11-27 11:24:45 +0530

    change banner style

--- src/Components/Donate/banner.jsx
+++ src/Components/Donate/banner.jsx
@@ -4,9 +4,9 @@ const Banner = () => {
     return (
         <div className="donate-container w-[100%]">
             <div className="title min-h-[200px] relative">
-                <div className="donate-overlay absolute inset-0 bg-green-600 opacity-[50%]"></div>
+                <div className="donate-overlay absolute inset-0 bg-black opacity-[55%]"></div>
                 <div className="absolute inset-0 flex items-center">
-                    <h1 className="header w-[100%] text-white text-6xl text-center">DONATE</h1>
+                    <h1 className="header w-[100%] text-gray-200 text-7xl text-center">DONATE</h1>
                     </div>
 
             </div>
